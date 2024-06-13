d4a17c4e4f9df5e7a5d4209e94d42d9b51ebbaf8
monnier david <david.monnier02@edu.univ-fcomte.fr>
2024-06-13 16:33:25 +0200

    Commit 1306 2

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends AbstractController
 {
